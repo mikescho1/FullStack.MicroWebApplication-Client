22b52f6c4320e501e623e45ba7488a5dc29b866a
mike <mike.schoenbeck@gmail.com>
2019-12-26 18:36:23 -0500

    added more empty model and service directories

--- MicroWebApplication-Client/src/app/app.module.ts
+++ MicroWebApplication-Client/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChatsComponent } from './chats/chats.component';
-import { MessageListComponent } from './chats/message-list/message-list.component';
-import { NewMessageComponent } from './chats/new-message/new-message.component';
-import { LoginComponent } from './login/login.component';
-import { AddUserComponent } from './user/add-user/add-user.component';
-import { EditUserComponent } from './user/edit-user/edit-user.component';
-import { UserListComponent } from './user/user-list/user-list.component';
+import { ChatsComponent } from './components/chat/chats.component';
+import { MessageListComponent } from './components/chat/message-list/message-list.component';
+import { NewMessageComponent } from './components/chat/new-message/new-message.component';
+import { LoginComponent } from './components/login/login.component';
+import { AddUserComponent } from './components/user/add-user/add-user.component';
+import { EditUserComponent } from './components/user/edit-user/edit-user.component';
+import { UserListComponent } from './components/user/user-list/user-list.component';
 
 @NgModule({
   declarations: [
