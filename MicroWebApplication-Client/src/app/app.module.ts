a46e72e0da7b7ae47f2409499dc0b7d81d5ef366
mike <mike.schoenbeck@gmail.com>
2019-12-26 18:21:50 -0500

    created login and user components, and empty model directories

--- MicroWebApplication-Client/src/app/app.module.ts
+++ MicroWebApplication-Client/src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { ChatsComponent } from './chats/chats.component';
 import { MessageListComponent } from './chats/message-list/message-list.component';
 import { NewMessageComponent } from './chats/new-message/new-message.component';
+import { LoginComponent } from './login/login.component';
+import { AddUserComponent } from './user/add-user/add-user.component';
+import { EditUserComponent } from './user/edit-user/edit-user.component';
+import { UserListComponent } from './user/user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatsComponent,
     MessageListComponent,
-    NewMessageComponent
+    NewMessageComponent,
+    LoginComponent,
+    AddUserComponent,
+    EditUserComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
