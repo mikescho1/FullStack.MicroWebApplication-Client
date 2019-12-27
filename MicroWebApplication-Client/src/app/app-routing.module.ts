fd4f9b304f48ac612eb7a428c552ba5fd70e153d
Ryan <ryan@rmiller.dev>
2019-12-27 12:08:01 -0500

    basic routing

--- MicroWebApplication-Client/src/app/app-routing.module.ts
+++ MicroWebApplication-Client/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {ChatComponent} from './components/chat/chat.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ChatComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
