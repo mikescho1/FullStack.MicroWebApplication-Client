31cbe9c2acaaf87d905a25232822c9c0d23e2951
mike <mike.schoenbeck@gmail.com>
2019-12-30 16:52:05 -0500

    created messageService class and basic crud operations

--- MicroWebApplication-Client/src/app/services/message.service.ts
+++ MicroWebApplication-Client/src/app/services/message.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
 
-  constructor() { }
-}
+  private baseUrl = '/messages'
+
+  constructor(private http: HttpClient) { }
+  getMessage(messageId: number): Observable<object> {
+    return this.http.get(`${this.baseUrl}/${messageId}`);
+  }
+
+  createMessage(message: Object): Observable<Object>  {
+    return this.http.post(`${this.baseUrl}`, message);
+  }
+
+  updateMessageName(messageId: number, message: string): Observable<Object> {
+    return this.http.put(`${this.baseUrl}/${messageId}`, message);
+  }
+
+  deleteMessage(messageId: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${messageId}`, {responseType: 'text' });
+  }
+
+  getMessageList(): Observable<any>  {
+    return this.http.get(`${this.baseUrl}`);
+  }
+
+
+
+
+
+} 
