0df68dd5579431adb09b894be964a01b1e66e171
Ryan <ryan@rmiller.dev>
2019-12-30 10:38:24 -0500

    main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HubComponent } from './hub/hub.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HubComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
