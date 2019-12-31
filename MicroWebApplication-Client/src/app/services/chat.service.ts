fabe013d8e11e5c66320e8956c791afbb8443773
mike <mike.schoenbeck@gmail.com>
2019-12-31 10:02:03 -0500

    add proxy.conf.json file to configure the server base url in a central place.

--- MicroWebApplication-Client/src/app/services/chat.service.ts
+++ MicroWebApplication-Client/src/app/services/chat.service.ts
@@ -15,18 +15,22 @@ export class ChatService {
     return this.http.get(`${this.baseUrl}/${chatId}`);
   }
 
+  // Needs logic to add creator as the admin.
   createChat(chat: Object): Observable<Object>  {
     return this.http.post(`${this.baseUrl}`, chat);
   }
 
+  // Needs logic to only allow admin to update name.
   updateChatName(chatId: number, newChatName: string): Observable<Object> {
     return this.http.put(`${this.baseUrl}/${chatId}`, newChatName);
   }
 
+  // Needs logic to only allow admin to delete.
   deleteChat(chatId: number): Observable<any> {
     return this.http.delete(`${this.baseUrl}/${chatId}`, {responseType: 'text' });
   }
 
+  // Needs logic to only get chats available to a particular user.
   getChatList(): Observable<any>  {
     return this.http.get(`${this.baseUrl}`);
   }
